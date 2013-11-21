3d5c0f8cc2125fff308ebb5f0ddfd0775d24dedf
Qi Li <cloudbsd@gmail.com>
2013-11-21 22:42:19 +0800

    initial import insurance project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Insurance::Application.config.secret_key_base = '46d3fb5e130a3f5f9ab060d7e00d3333cc49c83171c10b785034a27d325f7057838c759f5eff9f6698ef3d31eaf0b8c95600be26b3a631b832bf415780302aa0'
