666c7206f4fed12f08858c3867929821db01bf40
Qi Li <cloudbsd@gmail.com>
2013-11-22 23:46:49 +0800

    add a few locales and role_name() method for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   rolify
+
+  # instance methods
+  def role_name
+    return "role.name.broker" if self.has_role? :broker
+    return "role.name.customer" if self.has_role? :customer
+    return "role.name.moderator" if self.has_role? :moderator
+    return "role.name.admin" if self.has_role? :admin
+  end
 end
