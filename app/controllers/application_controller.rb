b2228ba5e9a8fed447c9afc26a58dc95d8444ed2
Qi Li <cloudbsd@gmail.com>
2013-11-22 23:24:27 +0800

    add authentication and locale features.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,108 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # set up user authentiation with devise
+  before_filter :authenticate_user!
+
+  before_filter :set_locale
+
+# before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  # devise_parameter_sanitizer.for(:sign_up) do |u|
+  #   u.permit(:email, :password, :password_confirmation, :profile_attributes => [:name])
+  #   u.permit!
+  # end
+
+    devise_parameter_sanitizer.for(:sign_up) << { :profile_attributes => [:name] }
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :current_password, :profile_attributes => [:name, :realname, :gravatar, :website, :location, :birthday, :aboutme, :id])
+    end
+  end
+
+  def set_locale
+    if params[:locale] and I18n.available_locales.include? params[:locale].to_sym
+      I18n.locale = params[:locale]
+    else
+      if params[:locale].present?
+        flash.now[:notice] = "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+      I18n.locale = I18n.default_locale
+    end
+  # I18n.locale = params[:locale] or I18n.default_locale
+  end
+
+  def default_url_options(options={})
+  # logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
+
+  def can?(action, object, parent=nil)
+    user = current_user
+
+    if (object.is_a? User)
+      if (action == :index or action == :show)
+        return true
+      elsif (user == object)
+        return true
+      else
+        return false
+      end
+    end
+
+    if (object.is_a? ActsAsTaggableOn::Tag)
+      if (action == :index)
+        return true
+      else
+        return false
+      end
+    end
+
+    if (action == :top)
+      return [Question, Answer, Comment].include?(object.class)
+    elsif (action == :index)
+      return [Question, Answer, Comment].include?(object.class)
+    elsif (action == :show)
+      return [Question, Answer, Comment].include?(object.class)
+    elsif !user_signed_in?
+      return false
+    elsif (action == :new)
+      if [Question, Answer].include?(object.class)
+        return true
+      elsif [Comment].include?(object.class)
+        return parent.owned_by?(user) || user.can_addcomment?
+      end
+    elsif (action == :create)
+      if [Question, Answer].include?(object.class)
+        return true
+      elsif [Comment].include?(object.class)
+        return parent.owned_by?(user) || user.can_addcomment?
+      end
+    elsif (action == :destroy)
+      return [Question, Answer, Comment].include?(object.class) && object.owned_by?(user)
+    elsif (action == :edit)
+      return [Question, Answer].include?(object.class) && object.owned_by?(user)
+    elsif (action == :update)
+      return [Question, Answer].include?(object.class) && object.owned_by?(user)
+    elsif (action == :upvote)
+      return [Question, Answer].include?(object.class) && !object.owned_by?(user) && user.can_upvote?
+    elsif (action == :downvote)
+      return [Question, Answer].include?(object.class) && !object.owned_by?(user) && user.can_downvote?
+    elsif (action == :favorite)
+      return [Question].include?(object.class)
+    elsif (action == :read)
+      return [Question].include?(object.class)
+    elsif (action == :accept)
+      return [Answer].include?(object.class)
+    else
+      return false
+    end
+  end
+
+  helper_method :can?
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+  skip_before_filter :authenticate_user!, :only => [:index, :show]
+
+# before_action :set_user, only: [:show, :edit, :update, :destroy, :upload_gravatar, :edit_gravatar, :update_gravatar, :edit_bio, :edit_aboutme, :edit_tags, :edit_password, :update_password]
+  before_action :set_user, except: [:index]
+
+# before_action :authorize_user!
+
+  # GET /users
+  # GET /users.json
   def index
+    @users = User.all
+#   @users = User.all.paginate(:page => params[:page], :per_page => 16)
   end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def authorize_user!
+      user = @user.nil? ? User.new : @user
+      unless can? params[:action].to_sym, user
+        redirect_to root_path, alert: 'You have no permission to access this page.'
+      end
+    end
 end
