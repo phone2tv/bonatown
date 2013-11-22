fd2d505335775dedf757e9a69db02a7b54b36942
Qi Li <cloudbsd@gmail.com>
2013-11-22 23:05:11 +0800

    create initial users by rake db:seed command.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-  rolify
+  # macros from gems
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  rolify
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def print_title(title)
+  puts "\n== #{title}"
+end
+
+def print_content(content, level = 1)
+  indent = '   ' * level
+  puts "#{indent}=> #{content}"
+end
+
+def create_users
+  print_title('setting up users')
+
+  # create admin
+  admin = User.create!(email: 'admin@example.com', password: 'password')
+  admin.add_role :admin
+# profile = create_profile(admin, 'Admin')
+  print_content "user: #{admin.email}"
+
+  # create moderator
+  moderator = User.create!(email: 'moderator@example.com', password: 'password')
+  moderator.add_role :moderator
+# profile = create_profile(moderator, 'Moderator')
+  print_content "user: #{moderator.email}"
+end
+
+def destroy_tables
+  print_title('destroy tables')
+  User.destroy_all
+  print_content "destroy all users"
+  Role.destroy_all
+  print_content "destroy all roles"
+end
+
+destroy_tables
+create_users
