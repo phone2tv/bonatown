49d73b8042ce493f6ae897120e74612decc366f2
Qi Li <cloudbsd@gmail.com>
2013-12-09 01:26:05 +0800

    add cart model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206152102) do
+ActiveRecord::Schema.define(version: 20131208172343) do
 
   create_table "accident_insurances", force: true do |t|
     t.text     "body"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20131206152102) do
   create_table "admin_profiles", force: true do |t|
     t.string   "name",       default: "", null: false
     t.text     "aboutme",    default: "", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "carts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
