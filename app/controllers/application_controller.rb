72fb01d6be7a87603334f19921dc5a0de8be9112
Qi Li <cloudbsd@gmail.com>
2013-12-09 01:31:01 +0800

    add current_cart controller private method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,16 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
   protected
 
   def configure_permitted_parameters
