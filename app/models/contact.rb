044def1cda480d0c9bcb8af9aabdc1f697f09162
Qi Li <cloudbsd@gmail.com>
2014-01-15 22:55:49 +0800

    implement gender in contact model with select control.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,11 @@
 class Contact < ActiveRecord::Base
   # constants
-  CARD_TYPE = [ "idcard", "armcard", "passport", "driving_license", "simplified_chinese", "other" ]
+  CARD_TYPE = [ 'idcard', 'armcard', 'passport', 'driving_license', 'simplified_chinese', 'other' ]
+  GENDER = [ ['secrecy', 's'], ['man', 'm'], ['woman', 'w'] ]
 
   # association macros
   belongs_to :customer_profile
+
+  # validation macros
+  validates :gender, presence: true, length: { is: 1 }
 end
